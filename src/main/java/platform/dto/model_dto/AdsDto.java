1df258d2442289001446654de32d18fddabec8ca
MaysVerrecas <99118212+MaysVerrecas@users.noreply.github.com>
2023-06-13 22:29:22 +0300

    Написаны дто для моделек

--- src/main/java/platform/dto/model_dto/AdsDto.java
+++ src/main/java/platform/dto/model_dto/AdsDto.java
@@ -1,32 +1,22 @@
 package platform.dto.model_dto;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
-/**
- * Обьект для вывода данных по результатам запросов
- */
-@Builder
-@Data
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class AdsDto {
 
-    @JsonProperty("pk")
-    private Integer pk;
-
-    @JsonProperty("author")
-    private Integer author;
-
-    @JsonProperty("price")
-    private Integer price;
-
-    @JsonProperty("title")
+    private int pk;
+    private int author;
+    private int price;
     private String title;
-
-    @JsonProperty("image")
-    @Valid
+    private String description;
     private String image;
 
 }
